2cba37284c39bdc5a2639f9927a8f9fc5dc8c75d
LauraC360 <63462151+LauraC360@users.noreply.github.com>
2025-01-04 15:55:55 +0200

    Update aggregate_test_data.py

--- src/aggregate_test_data.py
+++ src/aggregate_test_data.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 def aggregate_daily_data(data):
-    # Ensure the 'Data' column is in datetime format
+    # Transformare coloană Data în format datetime
     data['Data'] = pd.to_datetime(data['Data'], dayfirst=True)
 
-    # Convert relevant columns to numeric, coercing errors to NaN
+    # Convertirea coloanelor relevante la tipul numeric acolo unde e cazul
     columns_to_convert = [
         'Consum[MW]', 'Productie[MW]', 'Carbune[MW]', 'Hidrocarburi[MW]',
         'Ape[MW]', 'Nuclear[MW]', 'Eolian[MW]', 'Foto[MW]', 'Biomasa[MW]'
@@ -12,7 +12,7 @@ def aggregate_daily_data(data):
     for column in columns_to_convert:
         data[column] = pd.to_numeric(data[column], errors='coerce')
 
-    # Group by date and aggregate the features
+    # Gruparea pe zile și agregarea datelor
     daily_data = data.groupby(data['Data'].dt.date).agg({
         'Consum[MW]': 'sum',
         'Productie[MW]': 'sum',
@@ -25,18 +25,18 @@ def aggregate_daily_data(data):
         'Biomasa[MW]': 'sum'
     }).reset_index()
 
-    # Recalculate 'Sold[MW]' as the difference between 'Productie[MW]' and 'Consum[MW]'
+    # Recalculare 'Sold[MW]' ca diferența dintre 'Productie[MW]' și 'Consum[MW]'
     daily_data['Sold[MW]'] = daily_data['Productie[MW]'] - daily_data['Consum[MW]']
 
     return daily_data
 
-# Load the dataset
+# Citirea fișierului excel
 data_2024 = pd.read_excel('data/december_2024.xlsx')
 
-# Aggregate the data
+# Agregare date
 daily_data_2024 = aggregate_daily_data(data_2024)
 
-# Save the aggregated data to a new CSV file
+# Salvarea noilor date agregate într-un fișier CSV nou
 daily_data_2024.to_csv('data/daily_december_2024.csv', index=False)
 
 print("Aggregated data for December 2024 saved successfully.")
